6c86a105be7d5ccdea77509ec69a34696cfe56c0
PRADEEP MK <pradeepmk799@gmail.com>
2026-08-02 00:04:08 +0530

    config changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: true, // Enables source maps for production builds
+  },
   server: {
     port: 3000,
     host: true,
